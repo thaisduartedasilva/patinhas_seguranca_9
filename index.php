77d637364edc292e4c3bb6cb6ef84985cbf8df82
thais duarte <thais_duarte-silva@estudante.sesisenai.org.br>
2026-08-24 10:50:57 -0300

    add - index

---
+++ index.php
@@ -0,0 +1,26 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style/styles.css">
+    <title>Patinhas com segurança</title>
+</head>
+
+<body>
+
+    <header>
+    <h1>Pet Shop AUmigos:</h1>
+    </header>
+
+    <main>
+
+
+
+
+
+    
+    </main>
+
+
+</body>
+</html>
