15842707b8329dbfe1826cd419574df6f1a2a6a8
thais duarte <thais_duarte-silva@estudante.sesisenai.org.br>
2026-08-26 10:56:47 -0300

    add - cadastrar animal

---
+++ public/cadastrar_animal.php
@@ -0,0 +1,14 @@
+<?php
+
+include "../infra/conexao.php";
+
+$nome = $_POST["nome"];
+$raca = $_POST["raca"];
+$idade = $_POST["idade"];
+
+$sql = "INSERT INTO animais (nome, raca, idade) VALUES ('$nome', '$raca', '$idade')";
+
+mysqli_query($conexao, $sql);
+header("location: ../index.php");
+
+?>
